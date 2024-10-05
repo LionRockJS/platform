a317bae405db8da820bc54ddfb9bd648da50a644
Colin Leung <colin@cowise.co>
2024-10-05 13:33:55 +0800

    chore: update route

--- classes/serverAdapter/NodeHTTP.mjs
+++ classes/serverAdapter/NodeHTTP.mjs
@@ -1,4 +1,5 @@
-import { Central, RouteList } from '@lionrockjs/central';
+import { Central } from '@lionrockjs/central';
+import { RouteList } from '@lionrockjs/router';
 import RouteAdapter from "../routeAdapter/NodeHTTP.mjs";
 
 import http from 'node:http';
