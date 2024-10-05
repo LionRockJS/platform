446cd2b7bec86c31ce18918c5460c92e56428f11
Colin Leung <colin@cowise.co>
2024-10-05 13:32:36 +0800

    chore: update router

--- classes/serverAdapter/Express.mjs
+++ classes/serverAdapter/Express.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
-import { Central, RouteList } from '@lionrockjs/central';
+import { Central } from '@lionrockjs/central';
+import { RouteList } from '@lionrockjs/router';
 import RouteAdapter from "../routeAdapter/Express.mjs";
 import path from 'node:path';
 import cookieParser from 'cookie-parser';
