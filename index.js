09366c39d0b10c60aee056994fbd6d3e9ba03c08
Colin Leung <colin@cowise.co>
2026-05-14 04:00:47 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import ServerAdapterFastify from './classes/serverAdapter/Fastify.mjs';
-import RouterAdapterFastify from './classes/routeAdapter/Fastify.mjs';
-
-export {
-  ServerAdapterFastify,
-  RouterAdapterFastify,
-}
